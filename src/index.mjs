9d548a80210526d7fbc37abfbf2b86062ee21989
kt3t4 <satoshi.kaku@gmail.com>
2023-10-12 02:20:18 +0000

    Update index.mjs letは上書きOK。再宣言はできない。

--- src/index.mjs
+++ src/index.mjs
@@ -11,3 +11,12 @@
 // // var変数は再宣言可能
 // var val1 = "var変数を再宣言";
 // console.log(val1);
+
+let val2 = "let変数";
+console.log(val2);
+// letは上書き可能
+val2 = "let変数を上書き";
+console.log(val2);
+
+//  lete破砕宣言不可能
+let val2 = "let変数を再宣言";
